15b42e5435f30d44ef13f00f9c5272eaa4ff57f4
baku_nishiya <planet.of.early.summer.rain@gmail.com>
2023-01-09 23:12:44 +0900

    graphqlでポケモン図鑑作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./graphql/client";
+import { SearchResultField } from "./components/SearchResultField";
+import { SearchForm } from "./components/SearchForm";
 function App() {
+  const [pokemonName, setpokemonName] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SearchForm setpokemonName={setpokemonName}/>
+      <ApolloProvider client={apolloClient}>
+        {pokemonName && <SearchResultField pokemonName={pokemonName}/>}
+      </ApolloProvider>
+    </>
   );
 }
 
